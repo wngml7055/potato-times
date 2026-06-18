53bfb5ca96eed6461121124593ccfe1785d6c687
wngml7055 <wngml7055@gmail.com>
2026-06-18 13:35:31 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -182,7 +182,7 @@ with header_left:
             font-weight:bold;
             color:#203864;
             line-height:1.0;
-            margin-top:0px;
+            margin-top:-5px;
             margin-bottom:0px;
             white-space:nowrap;
         ">
