da16cd17fbfce94ec70ae793d06b8888852bfa5a
wngml7055 <wngml7055@gmail.com>
2026-06-18 12:55:54 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -253,7 +253,7 @@ if os.path.exists("data/kamis_potato.csv"):
 <div style="
     background:#EDF4FF;
     border-left:4px solid #4A90E2;
-    padding:4px 12px;
+    padding:4px 4px;
     border-radius:6px;
     margin-top:0px;
     margin-bottom:0px;
