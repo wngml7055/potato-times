8f631d5a0678a797202d427c11bf18f6410154d3
wngml7055 <wngml7055@gmail.com>
2026-06-18 12:46:53 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -245,8 +245,8 @@ if os.path.exists("data/kamis_potato.csv"):
             latest["content"]
         )
 
-        title_size = "15px" if mobile else "15px"
-        body_size = "14px" if mobile else "14px"
+        title_size = "15px" if mobile else "14px"
+        body_size = "14px" if mobile else "13px"
 
         st.markdown(
             f"""
