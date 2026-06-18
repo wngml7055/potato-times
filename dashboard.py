ee54b5a166b4a69a39f09633cc53b75c46e7a609
wngml7055 <wngml7055@gmail.com>
2026-06-18 09:31:44 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -148,6 +148,7 @@ with header_left:
         margin-bottom:0px;
     ">
         🥔 Potato Times
+        </div>
     """, unsafe_allow_html=True)
 
 with header_right:
@@ -167,6 +168,7 @@ with header_right:
         ">
             정상<br>
             {datetime.today().strftime("%Y-%m-%d")}
+            </div>
         """,
         unsafe_allow_html=True
     )
